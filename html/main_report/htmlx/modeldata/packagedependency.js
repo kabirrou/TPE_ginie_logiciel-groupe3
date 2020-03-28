49d0c75435031a34ac0f6a9ffdc59ac355aedaf0
svr <gnu@debian-gnu>
2020-03-28 21:59:17 +0100

    Adding new file from Debian GNU

---
+++ html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,20 @@
+var matrix = [[0,0,0,0,0,0],[2,0,0,0,0,1],[1,0,0,0,0,0],[1,1,1,0,0,0],[1,1,1,0,0,0],[2,0,0,0,0,0]];
+var packages = [{
+"name": " org.fs.sled.entity", "color": " #3182bd"
+}
+,{
+"name": " org.fs.sled.metier", "color": " #6baed6"
+}
+,{
+"name": " org.fs.sled.model", "color": " #9ecae1"
+}
+,{
+"name": " org.fs.sled.controllers", "color": " #c6dbef"
+}
+,{
+"name": " org.fs.sled", "color": " #e6550d"
+}
+,{
+"name": " org.fs.sled.dao", "color": " #fd8d3c"
+}
+];
