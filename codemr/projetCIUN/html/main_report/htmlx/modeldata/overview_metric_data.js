6129bfcf3b0a808bfe31fbc3425b9a759ad0d60d
svr <gnu@debian-gnu>
2020-03-30 09:43:14 +0100

    Ajout d'un autre dossier prealablement mal envoye

---
+++ codemr/projetCIUN/html/main_report/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"projetCIUN", "Number of Highly Problematic Classes":0, "Number of Entities":15, "Number of Problematic Classes":1, "Class Lines of Code":1054, "Number of External Packages":8, "Number of Packages":6, "Number of External Entities":18}
